ccf6ffff47cd02c862dca4f8538081c168a19d08
LindaOuer <linda.ouerfelli@gmail.com>
2026-01-31 10:43:31 +0100

    Admin - first steps personnalization

--- ConferenceApp/admin.py
+++ ConferenceApp/admin.py
@@ -2,6 +2,46 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Conference)
-admin.site.register(Submission)
+
+class ConferenceAdmin(admin.ModelAdmin):
+    list_display= ('name', 'location', 'start_date', 'end_date')
+    search_fields = ('name', 'theme', 'location') # Ajout d'un champ de recherche
+    list_filter = ('theme', 'location') # Ajout de filtres pour faciliter la navigation
+    list_per_page = 1  # Pagination pour afficher 10 conférences par page
+    ordering = ('-start_date',)  # Trier par date de début décroissante
+    
+
+admin.site.register(Conference, ConferenceAdmin)
+
+@admin.register(Submission)
+class SubmissionAdmin(admin.ModelAdmin):
+    actions = ['set_payed']
+    
+    def set_payed(self, request, queryset):
+        queryset.update(payed=True)
+        self.message_user(request, "Selected submissions have been marked as payed.")
+    set_payed.short_description = "Mark selected submissions as payed"
+    
+    list_display = ('title', 'conference', 'user', 'submission_date', 'status', 'payed')
+    search_fields = ('title', 'user__username', 'conference__name')
+    list_filter = ('status', 'payed', 'conference__theme')
+    list_per_page = 10
+    ordering = ('-submission_date', 'title')
+    list_editable = ('status',)
+    
+    # fields = ('title', 'abstract', 'paper', 'keywords', 'status', 'payed', 'conference', 'user')   
+    fieldsets = (
+        ('Submission Details', {  
+            'fields': ('title', 'abstract', 'paper', 'keywords')
+        }),
+        ('Status Information', {
+            'fields': ('status', 'payed')
+        }),
+        ('Associations', {
+            'fields': ('conference', 'user')
+        }),
+    )
+    
+
+
 admin.site.register(OrganizingCommittee)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from fpw-2526-4cinfogl!")
+
+
+if __name__ == "__main__":
+    main()
